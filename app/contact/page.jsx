b6c6c6af7f3294b2a5366f4748db217cab3318e1
the-dg04 <devgup04@gmail.com>
2024-12-28 15:28:05 +0530

    contact us fix

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -23,11 +23,11 @@ export default function Page() {
           <div className="w-full md:w-1/2 h-1/2 md:h-auto p-6 lg:p-20 flex items-center justify-center">
             <motion.form className="bg-[#3A6D8C] p-10 rounded-lg w-full text-[#EAD8B1] flex flex-col items-center gap-2 relative" initial={{y:50,opacity:0}} whileInView={{y:0,opacity:1}}>
                 <div className="text-md w-full">Email</div>
-                <input type="text" className="w-full h-10 bg-transparent border border-[#EAD8B1]"/>
+                <input type="text" className="w-full h-10 bg-transparent border-b border-[#EAD8B1] mb-2" />
                 <div className="text-md w-full">Phone</div>
-                <input type="text" className="w-full h-10 bg-transparent border border-[#EAD8B1]"/>
+                <input type="text" className="w-full h-10 bg-transparent border-b border-[#EAD8B1] mb-2"/>
                 <div className="text-md w-full">Message</div>
-                <textarea type="text" className="w-full h-20 bg-transparent border border-[#EAD8B1]"/>
+                <textarea type="text" className="w-full h-20 bg-transparent border-b border-[#EAD8B1]"/>
                 <button type="submit" className="my-2 px-6 py-2 bg-[#001F3F] text-lg rounded-lg hover:text-black hover:bg-[#EAD8B1] transition-all duration-500">Submit</button>
                 <motion.div className="absolute w-[100px] h-[100px] bg-[#EAD8B1] rounded-full top-[-50px] right-[calc(50%-50px)] opacity-60 pointer-events-none" initial={{scale:0}} whileInView={{scale:1}} transition={{delay:0.2}}></motion.div>
             </motion.form>
