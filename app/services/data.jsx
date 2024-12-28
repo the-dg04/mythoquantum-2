db81f0cb18c7d26d59bc5578ddc9befdabc0dec2
the-dg04 <devgup04@gmail.com>
2024-12-28 17:22:14 +0530

    added services data

--- app/services/data.jsx
+++ app/services/data.jsx
@@ -6,46 +6,45 @@ export const ServicesData = [
     description:
       "Harness the power of data analytics and AI to revolutionize finance, healthcare, and supply chain operations. Our solutions are designed to unlock actionable insights, optimize processes, and drive digital transformation.",
     capabilities: [
-      "Capability 1",
-      "Capability 2",
-      "Capability 3",
-      "Capability 4",
+      "Predictive analytics for market trends and financial planning.",
+      "AI-powered diagnostics and patient management in healthcare.",
+      "Advanced forecasting and demand planning for supply chains.",
     ],
   },
   {
     img_src: "https://placehold.co/600x400",
     alt: "Slide 2",
-    name: "Service Name",
-    description: "Description",
+    name: "Data Modeling",
+    description:
+      "Our data modeling capabilities turn complex datasets into clear strategies that empower businesses. Whether it’s financial projections, healthcare analytics, or supply chain optimization, we create models that drive results.",
     capabilities: [
-      "Capability 1",
-      "Capability 2",
-      "Capability 3",
-      "Capability 4",
+      "Financial modeling for investment and risk analysis.",
+      "Healthcare data integration for improved outcomes.",
+      "Supply chain optimization through advanced simulation models.",
     ],
   },
   {
     img_src: "https://placehold.co/600x400",
     alt: "Slide 3",
-    name: "Service Name",
-    description: "Description",
+    name: "Development",
+    description:
+      "From concept to deployment, our development expertise ensures impactful solutions tailored to your industry. Whether automating financial systems, creating healthcare platforms, or optimizing supply chain workflows, we deliver results.",
     capabilities: [
-      "Capability 1",
-      "Capability 2",
-      "Capability 3",
-      "Capability 4",
+      "End-to-end financial application development.",
+      "Scalable healthcare platforms for improved accessibility.",
+      "Workflow automation for seamless supply chain operations.",
     ],
   },
-  {
-    img_src: "https://placehold.co/600x400",
-    alt: "Slide 4",
-    name: "Service Name",
-    description: "Description",
-    capabilities: [
-      "Capability 1",
-      "Capability 2",
-      "Capability 3",
-      "Capability 4",
-    ],
-  },
+  //   {
+  //     img_src: "https://placehold.co/600x400",
+  //     alt: "Slide 4",
+  //     name: "Service Name",
+  //     description: "Description",
+  //     capabilities: [
+  //       "Capability 1",
+  //       "Capability 2",
+  //       "Capability 3",
+  //       "Capability 4",
+  //     ],
+  //   },
 ];
